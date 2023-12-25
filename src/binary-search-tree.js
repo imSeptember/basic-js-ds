070aa73ed1c7dcb682d847de7351c6c0ed0804e4
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-12-25 17:04:30 +0200

    1

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,141 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.leftChild = null;
+    this.rightChild = null;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insert(value) {
+    const newNode = new TreeNode(value);
+
+    if (!this.rootNode) {
+      this.rootNode = newNode;
+    } else {
+      this.insertNode(this.rootNode, newNode);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insertNode(parent, newNode) {
+    if (newNode.value < parent.value) {
+      if (!parent.leftChild) {
+        parent.leftChild = newNode;
+      } else {
+        this.insertNode(parent.leftChild, newNode);
+      }
+    } else {
+      if (!parent.rightChild) {
+        parent.rightChild = newNode;
+      } else {
+        this.insertNode(parent.rightChild, newNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  contains(value) {
+    return this.search(this.rootNode, value);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  search(node, value) {
+    if (!node) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.search(node.leftChild, value);
+    } else if (value > node.value) {
+      return this.search(node.rightChild, value);
+    } else {
+      return true;
+    }
+  }
+
+  findNode(value) {
+    return this.find(this.rootNode, value);
+  }
+
+  find(node, value) {
+    if (!node) {
+      return null;
+    }
+
+    if (value < node.value) {
+      return this.find(node.leftChild, value);
+    } else if (value > node.value) {
+      return this.find(node.rightChild, value);
+    } else {
+      return node;
+    }
+  }
+
+  remove(value) {
+    this.rootNode = this.removeNode(this.rootNode, value);
+  }
+
+  removeNode(node, value) {
+    if (!node) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.leftChild = this.removeNode(node.leftChild, value);
+    } else if (value > node.value) {
+      node.rightChild = this.removeNode(node.rightChild, value);
+    } else {
+      if (!node.leftChild && !node.rightChild) {
+        return null;
+      } else if (!node.leftChild) {
+        return node.rightChild;
+      } else if (!node.rightChild) {
+        return node.leftChild;
+      } else {
+        const minValue = this.findMin(node.rightChild);
+        node.value = minValue;
+        node.rightChild = this.removeNode(node.rightChild, minValue);
+      }
+    }
+
+    return node;
+  }
+
+  findMin(node) {
+    while (node.leftChild) {
+      node = node.leftChild;
+    }
+    return node.value;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+    return this.findMin(this.rootNode);
+  }
+
+  findMax(node) {
+    while (node.rightChild) {
+      node = node.rightChild;
+    }
+    return node.value;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+    return this.findMax(this.rootNode);
   }
 }
 
